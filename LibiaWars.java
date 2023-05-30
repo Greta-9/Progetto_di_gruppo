de91d6545db7f55aa261eff9193d4f7718467a80
Hollow126 <tizigess@gmail.com>
2023-05-30 12:59:51 +0200

    prova main

---
+++ LibiaWars.java
@@ -0,0 +1,19 @@
+package Es22;
+
+public class LibiaWars 
+{
+    public static void main(String[] args) 
+    {
+        int colonne = 3;
+        int righe=24;
+        String [][] territori = new String [righe][colonne]; 
+        
+        for (int i = 0; i < 10; i++) 
+        {
+            System.out.println("che cosa vuoi fare");
+
+        }
+    }   
+    
+    
+}
